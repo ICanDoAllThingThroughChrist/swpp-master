1212fd1e312c82f54e5cdd0ef99acfc014bcd354
github username <github email address>
2017-12-18 20:45:14 -0600

    add_a_postgres_db

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,20 @@
-ENV['SINATRA_ENV'] ||= "development"
+require 'sinatra/activerecord'
 
-require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
+#The environment variable DATABASE_URL should be in the following format:
+# => postgres://{user}:{password}@{host}:{port}/path
+configure :production, :development do
+  db = URI.parse(ENV['DATABASE_URL'] || "postgres://#{ENV['DATA_DB_USER']}:#{ENV['DATA_DB_PASS']}@#{ENV['DATA_DB_HOST']}/gonano")
 
-ActiveRecord::Base.establish_connection(#CONNECTING TO THE DATABASE
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
+#https://guides.nanobox.io/ruby/sinatra/add-a-database/
 
 require_all 'app'
 #adapter: postgresql
