d7404180b47ebcb8f04290b4e41e435bc1c15976
github username <github email address>
2017-12-18 20:04:13 -0600

    checked pg is in Gemfile

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,3 @@
-# ENV['SINATRA_ENV'] ||= "development"
-
-# require 'bundler/setup'
-# Bundler.require(:default, ENV['SINATRA_ENV'])
-
-# ActiveRecord::Base.establish_connection(#CONNECTING TO THE DATABASE
-#   :adapter => "sqlite3",
-#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-# )
-#The environment variable DATABASE_URL should be in the following format:
-# => postgres://{user}:{password}@{host}:{port}/path
 
 configure :production, :development do
 	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/sinatraproject')
@@ -24,16 +13,3 @@ configure :production, :development do
 end
 
 require_all 'app'
-#adapter: postgresql
-#https://th3silverlining.com/2012/04/22/using-the-heroku-shared-database-with-sinatra-and-active-record/
-# ENV['SINATRA_ENV'] ||= "development"
-
-# require 'bundler/setup'
-# Bundler.require(:default, ENV['SINATRA_ENV'])
-
-# ActiveRecord::Base.establish_connection(
-#   :adapter => "sqlite3",
-#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-# )
-
-# require_all 'app'
