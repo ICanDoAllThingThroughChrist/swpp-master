1b89b33917aceb2b4cc1cecd42994bef74d1225e
github username <github email address>
2017-12-18 19:56:39 -0600

    checked pg is in Gemfile

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,27 @@
-ENV['SINATRA_ENV'] ||= "development"
+# ENV['SINATRA_ENV'] ||= "development"
 
-require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
+# require 'bundler/setup'
+# Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(#CONNECTING TO THE DATABASE
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+# ActiveRecord::Base.establish_connection(#CONNECTING TO THE DATABASE
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
+#The environment variable DATABASE_URL should be in the following format:
+# => postgres://{user}:{password}@{host}:{port}/path
+
+configure :production, :development do
+	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/sinatraproject')
+
+	ActiveRecord::Base.establish_connection(
+			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+			:host     => db.host,
+			:username => db.user,
+			:password => db.password,
+			:database => db.path[1..-1],
+			:encoding => 'utf8'
+	)
+end
 
 require_all 'app'
 #adapter: postgresql
